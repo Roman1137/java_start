ebc14b81b0574d47c037d6a508dd0e9d5db3c9cf
Roman <poma.borodavka@gmail.com>
2018-05-19 21:47:03 +0300

    test commit

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,19 +8,19 @@ public class MyFirstProgram {
     double l = 5;
     System.out.println("Площадь квардата со стороной " + l + " равна" + area(l));
 
-    double a =5,b=6;
-    System.out.println("Площадь прямоугольника со сторонами "+ a + " и " + b+ " = "+ area(a,b));
+    double a = 5, b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
   }
 
   public static void hello(String name) {
     System.out.println("Hello, " + name);
   }
 
-  public static double area (double l){
-    return l*l;
+  public static double area(double l) {
+    return l * l;
   }
 
-  public static double area (double a, double b){
-    return a*b;
+  public static double area(double a, double b) {
+    return a * b;
   }
 }
