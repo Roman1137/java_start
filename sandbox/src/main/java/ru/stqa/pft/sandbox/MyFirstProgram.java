0020916403f828d715e742bcd2257577a532d139
Roman <poma.borodavka@gmail.com>
2018-05-19 21:22:33 +0300

    lesson 12 was finished

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,22 +1,28 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, world+1");
+  public static void main(String[] args) {
+    System.out.println("Hello, world+1");
 
-      System.out.println(2+2);
-      System.out.println(2*2);
-      System.out.println(2/2);
-      System.out.println(2-2);
+    System.out.println(2 + 2);
+    System.out.println(2 * 2);
+    System.out.println(2 / 2);
+    System.out.println(2 - 2);
 
-      System.out.println(1.0/2);
-      System.out.println(1/2.0);
-      System.out.println(2.0/2);
+    System.out.println(1.0 / 2);
+    System.out.println(1 / 2.0);
+    System.out.println(2.0 / 2);
 
-      System.out.println("2"+"2");
-      System.out.println("2"+2);
+    System.out.println("2" + "2");
+    System.out.println("2" + 2);
 
-      System.out.println(2+2*2);
-      System.out.println((2+2)*2);
-    }
+    System.out.println(2 + 2 * 2);
+    System.out.println((2 + 2) * 2);
+    System.out.println("Hello, " + "world+1");
+
+    double l = 8;
+    String a = "Hello, world";
+    System.out.println(a);
+    System.out.println("Площадь квардата со стороной " + l + "=" + (l * l));
+  }
 }
