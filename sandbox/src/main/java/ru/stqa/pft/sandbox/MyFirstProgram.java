b15c38dff8645db858ca0eda10126c5fc36a9d2e
Roman <poma.borodavka@gmail.com>
2018-05-19 22:37:52 +0300

    lection 16 was finished

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,23 +6,14 @@ public class MyFirstProgram {
     hello(name);
 
     Square s = new Square(5);
-    System.out.println("Площадь квардата со стороной " + s.l + " равна " + area(s));
+    System.out.println("Площадь квардата со стороной " + s.l + " равна " + s.area());
 
 
     Rectangle r = new Rectangle(4,6);
-
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 }
 
   public static void hello(String name) {
     System.out.println("Hello, " + name);
-  }
-
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
   }
 }
